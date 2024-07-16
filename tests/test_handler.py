ea70fe09dd11976de44fd5d0c531f2169f37f0c9
Nattan Karrary <74218114+Karrary37@users.noreply.github.com>
2024-07-16 16:26:44 -0300

    pre-commit

---
+++ tests/test_handler.py
@@ -0,0 +1,27 @@
+import unittest
+import json
+from handler import hello
+
+
+class TestHelloFunction(unittest.TestCase):
+
+    def test_hello(self):
+        event = {}
+        context = {}
+        response = hello(event, context)
+
+        self.assertEqual(response["statusCode"], 200)
+        body = json.loads(response["body"])
+        self.assertEqual(body["message"], "Hello, World!")
+        self.assertEqual(body["input"], event)
+
+    def test_hello_with_event(self):
+        event = {"key": "value"}
+        context = {}
+        response = hello(event, context)
+
+        self.assertEqual(response["statusCode"], 200)
+        body = json.loads(response["body"])
+        self.assertEqual(body["message"], "Hello, World!")
+        self.assertEqual(body["input"], event)
+
